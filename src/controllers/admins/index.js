0f10886d749acfccba293e86b1ea72a426f28b08
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2026-01-02 23:30:38 +0530

    feat(controllers): Update admin controllers exports
    - Remove role-change-request controllers (over-engineered)
    - Export all status request controllers (8 total)
    - Export viewStatusRequest, listAdmins, viewOwnAdminDetails
    - Total: 18 admin controllers exported

--- src/controllers/admins/index.js
+++ src/controllers/admins/index.js
@@ -2,10 +2,24 @@ const { activateAdmin } = require("./activate-admin.controller");
 const { createAdmin } = require("./create-admin.controller");
 const { bulkAdminCreate } = require("./create-admins-in-bulk.controller");
 const { deactivateAdmin } = require("./deactivate-admin.controller");
-const { submitRoleChangeRequest, rejectRoleChangeRequest, approveRoleChangeRequest } = require("./role-change-request.controller");
 const { updateAdminDetails } = require("./update-admin-details.controller");
 const { updateOwnAdminDetails } = require("./update-own-admin-details.controller");
 const { viewAdminDetails } = require("./view-admin-details.controller");
+const { viewOwnAdminDetails } = require("./view-own-admin-details.controller");
+const { listAdmins } = require("./list-admins.controller");
+
+// Status Request Controllers
+const { createDeactivationRequest } = require("./create-deactivation-request.controller");
+const { listDeactivationRequests } = require("./list-deactivation-requests.controller");
+const { approveDeactivationRequest } = require("./approve-deactivation-request.controller");
+const { rejectDeactivationRequest } = require("./reject-deactivation-request.controller");
+
+const { createActivationRequest } = require("./create-activation-request.controller");
+const { listActivationRequests } = require("./list-activation-requests.controller");
+const { approveActivationRequest } = require("./approve-activation-request.controller");
+const { rejectActivationRequest } = require("./reject-activation-request.controller");
+
+const { viewStatusRequest } = require("./view-status-request.controller");
 
 const adminControllers = {
     createAdmin,
@@ -14,10 +28,20 @@ const adminControllers = {
     deactivateAdmin,
     updateAdminDetails,
     viewAdminDetails,
-    submitRoleChangeRequest,
-    rejectRoleChangeRequest,
-    approveRoleChangeRequest,
-    updateOwnAdminDetails
+    viewOwnAdminDetails,
+    listAdmins,
+    updateOwnAdminDetails,
+    
+    // Status Requests
+    createDeactivationRequest,
+    listDeactivationRequests,
+    approveDeactivationRequest,
+    rejectDeactivationRequest,
+    createActivationRequest,
+    listActivationRequests,
+    approveActivationRequest,
+    rejectActivationRequest,
+    viewStatusRequest
 }
 
 module.exports = {
