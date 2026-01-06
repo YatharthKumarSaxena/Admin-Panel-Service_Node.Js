40254c6c58bd9b1e606751b4763b2381fb1fc4a7
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2026-01-06 11:36:50 +0530

    URIs are Updated

--- src/configs/uri.config.js
+++ src/configs/uri.config.js
@@ -38,9 +38,6 @@ module.exports = {
     USER_ROUTES: {
         BLOCK_USER: `/block-user`,              // PATCH /admin-panel-service/api/v1/users/block-user
         UNBLOCK_USER: `/unblock-user`,          // PATCH /admin-panel-service/api/v1/users/unblock-user
-        GET_USER_ACTIVE_SESSIONS: `/active-sessions`,   // GET /admin-panel-service/api/v1/users/active-sessions
-        FETCH_USER_DETAILS: `/fetch-user-details`,       // GET /admin-panel-service/api/v1/users/fetch-user-details
-        GET_USER_AUTH_LOGS: `/auth-logs`,  // GET /admin-panel-service/api/v1/users/auth-logs
         GET_TOTAL_REGISTERED_USERS: `/total-users`,      // GET /admin-panel-service/api/v1/users/total-users
         LIST_USERS: `/list`                     // GET /admin-panel-service/api/v1/users/list
 
@@ -49,7 +46,10 @@ module.exports = {
     INTERNAL_ROUTES: {
         SYNC_USER_DATA: `/sync-user-data`,
         BLOCK_DEVICE: `/block-device`,
-        UNBLOCK_DEVICE: `/unblock-device`
+        UNBLOCK_DEVICE: `/unblock-device`,
+        GET_USER_ACTIVE_SESSIONS: `/active-sessions`,   // GET /admin-panel-service/api/v1/users/active-sessions
+        FETCH_USER_DETAILS: `/fetch-user-details`,       // GET /admin-panel-service/api/v1/users/fetch-user-details
+        GET_USER_AUTH_LOGS: `/auth-logs`,  // GET /admin-panel-service/api/v1/users/auth-logs
     },
 
     REQUEST_ROUTES: {
