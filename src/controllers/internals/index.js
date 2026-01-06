6b249d9dce19d7fb112a4b719848e1a232fa3aaf
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2026-01-06 15:46:57 +0530

    Refined Internal Folder Structure Making It Ready so that it can interact with other Backend Service Like Auth

--- src/controllers/internals/index.js
+++ src/controllers/internals/index.js
@@ -1,9 +1,9 @@
-const { listAuthLogs } = require("./list-auth-logs");
-const { blockDevice } = require("./block-device.controller");
-const { unblockDevice } = require("./unblock-device.controller");
-const { checkAuthLogs } = require("./check-auth-logs.controller");
-const { getUserActiveDevices } = require("./get-user-active-devices.controller");
-const { provideUserAccountDetails } = require("./provide-user-account-details.controller");
+const { listAuthLogs } = require("./auth-service/data-queries/get/list-auth-logs");
+const { blockDevice } = require("./auth-service/data-queries/update/block-device.controller");
+const { unblockDevice } = require("./auth-service/data-queries/update/unblock-device.controller");
+const { checkAuthLogs } = require("./auth-service/data-queries/get/check-auth-logs.controller");
+const { getUserActiveDevices } = require("./auth-service/data-queries/get/get-user-active-devices.controller");
+const { provideUserAccountDetails } = require("./auth-service/data-queries/get/provide-user-account-details.controller");
 
 const internalControllers = {
   listAuthLogs,
