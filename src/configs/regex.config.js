331aad0d55cb102f8b333b5ecf904f4b5bbb26e1
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2026-02-14 22:30:29 +0530

    refactor(config): Update regex patterns for phone number validation
    - Add countryCodeRegex for validating country codes (1-3 digits, starting with 1-9)
    - Add localNumberRegex for validating local phone numbers (9-12 digits)
    - Rename fullPhoneNumberRegex to phoneNumberRegex for clarity
    - Remove overrideIdRegex (feature deprecated)
    - Improve phone number validation granularity and flexibility

--- src/configs/regex.config.js
+++ src/configs/regex.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    fullPhoneNumberRegex: /^\+([1-9]\d{0,2})(\d{9,12})$/,
+    countryCodeRegex: /^[1-9]\d{0,2}$/,
+    localNumberRegex: /^\d{9,12}$/,
+    phoneNumberRegex: /^\+([1-9]\d{0,2})(\d{9,12})$/,
     emailRegex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
     firstNameRegex: /^[a-zA-Z\s'-]+$/,
     UUID_V4_REGEX: /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
     mongoIdRegex: /^[a-f\d]{24}$/i,
     adminIdRegex: /^ADM[0-9]{7}$/,
     userIdRegex: /^USR[0-9]{7}$/,
-    requestIdRegex: /^REQ[0-9]{10}$/,
-    overrideIdRegex: /^OVR[0-9]{10}$/
+    requestIdRegex: /^REQ[0-9]{10}$/
 }
